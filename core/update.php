1787f5efe7d70e7dacf8d18748c96d0286ffb8c4
D <danixoon@gmail.com>
2020-06-01 02:47:44 +0300

    debug

--- core/update.php
+++ core/update.php
@@ -88,10 +88,12 @@ function handle_get()
     $sql = "UPDATE precinct SET $sql_values WHERE id='$id'";
     $result = $mysqli->query($sql);
 
-    send_data("Адрес: " . $geocode);
+    
 
     if (!$result) {
         send_data("Произошла ошибка: " . $mysqli->error . "\n" . "Запрос: $sql", 500);
         return;
     };
+
+    send_data($response_data);
 }
